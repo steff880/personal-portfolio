52d894a21c305d5db579586cf33698e0d51ab49e
Stefan <stefan.d.yordanov@gmail.com>
2024-09-12 22:09:30 +0100

    Slightly increase the size of SliderButton on larger screens

--- src/components/Testimonials/SliderButton.jsx
+++ src/components/Testimonials/SliderButton.jsx
@@ -2,7 +2,7 @@ const SliderButton = ({ children, handleOnClick, position }) => {
   return (
     <button
       onClick={handleOnClick}
-      className={`${position} flex h-8 w-8 items-center justify-center rounded-full text-[1rem] transition-[background] duration-200 ease-in-out hover:bg-[var(--purple)]`}
+      className={`${position} flex h-8 w-8 items-center justify-center rounded-full text-[1rem] transition-[background] duration-200 ease-in-out hover:bg-[var(--purple)] sm:right-0 md:right-5 md:h-10 md:w-10 md:text-[1.1rem] lg:right-10 xl:right-28`}
     >
       {children}
     </button>
